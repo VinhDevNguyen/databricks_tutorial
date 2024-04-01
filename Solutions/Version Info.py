5b1e430d949cea86e1aa9fdb6bdd34f036178a64
VincentGL <vinh2000ld14@gmail.com>
2024-04-01 10:11:53 +0700

    Init commit

---
+++ Solutions/Version Info.py
@@ -0,0 +1,30 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+<div style="text-align: center; line-height: 0; padding-top: 9px;">
+  <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning">
+</div>
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC
+# MAGIC
+# MAGIC # Project Information
+# MAGIC
+# MAGIC * Name: **Advanced Data Engineering With Databricks**
+# MAGIC * Version:  **3.4.0**
+# MAGIC * Built On: **March 07 2024 03:32:09**
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+&copy; 2024 Databricks, Inc. All rights reserved.<br/>
+Apache, Apache Spark, Spark and the Spark logo are trademarks of the 
+<a href="https://www.apache.org/">Apache Software Foundation</a>.<br/>
+<br/><a href="https://databricks.com/privacy-policy">Privacy Policy</a> | 
+<a href="https://databricks.com/terms-of-use">Terms of Use</a> | 
+<a href="https://help.databricks.com/">Support</a>
